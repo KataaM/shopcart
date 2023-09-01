f7078e26656d0e0254ec0398985869654119d19a
KataM <gregory.fazzino@gmail.com>
2023-09-01 12:05:41 +0200

    Fin de la banniere sans animation ni interaction

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,12 +1,4 @@
-<script>
-// import Searchbar from './searchbar.vue'
-
-// export default {
-//   components: {
-//     Searchbar
-//   }
-// }
-</script>
+<script></script>
 
 <template>
   <section class="banner">
@@ -27,16 +19,73 @@
         <div class="button wrapper"><a class="button-text" href="#">Learn More</a></div>
       </div>
     </div>
+    <div class="banner-stage-wrap">
+      <div class="banner-stage-bg">
+        <img src="../assets/images/banner-yellow-stairs.png" alt="" />
+      </div>
+      <div class="product-image-one">
+        <img src="../assets/images/product-image-1.png" alt="" />
+      </div>
+      <div class="product-image-two">
+        <img src="../assets/images/product-image-2.png" alt="" />
+      </div>
+      <div class="product-image-three">
+        <img src="../assets/images/product-image-3.png" alt="" />
+      </div>
+      <div class="product-image-four">
+        <img src="../assets/images/product-image-4.png" alt="" />
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped>
-.container {
-  max-width: 1360px;
-  padding-left: 40px;
-  padding-right: 40px;
-  margin-right: auto;
-  margin-left: auto;
+.product-image-two {
+  position: absolute;
+  left: auto;
+  top: 156px;
+  right: 433px;
+  bottom: auto;
+}
+.product-image-three {
+  position: absolute;
+  left: auto;
+  top: 300px;
+  right: 208px;
+  bottom: auto;
+}
+.product-image-four {
+  position: absolute;
+  left: auto;
+  top: 350px;
+  right: 550px;
+  bottom: auto;
+}
+.product-image-one {
+  position: absolute;
+  left: auto;
+  top: 71px;
+  right: 125px;
+  bottom: auto;
+}
+.banner-stage-bg {
+  text-align: right;
+}
+img {
+  max-width: 100%;
+  vertical-align: middle;
+  display: inline-block;
+  text-align: right;
+}
+.banner-stage-wrap {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  right: auto;
+  bottom: auto;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
 }
 
 .button-text {
@@ -52,6 +101,7 @@
 .banner-button {
   width: 100%;
   height: 100%;
+  padding-top: 16px;
 }
 .banner-content-paragraph {
   max-width: 495px;
@@ -63,6 +113,7 @@
 .banner-content-title {
   color: #003d29;
   text-transform: capitalize;
+  margin-bottom: 30px;
 }
 
 h1 {
@@ -78,7 +129,7 @@ h1 {
   width: 100%;
   z-index: 0;
   padding-top: 150px;
-  padding-bottom: 150px;
+  padding-bottom: 180px;
   background-image: url('../assets/images/banner-bg.png');
   background-position: 50% 50%;
   background-size: cover;
