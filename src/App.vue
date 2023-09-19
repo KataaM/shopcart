5254a48b4777c8a0aafa5755a3b48866f7d08991
KataM <gregory.fazzino@gmail.com>
2023-09-19 12:36:03 +0200

    Add categories and ugly slider + add brand section (todo hover etc.. animation)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import CategoryCard from './components/CategoryCard.vue'
 import HeroBanner from './components/HeroBanner.vue'
 import Navbar from './components/navbar/Navbar.vue'
 import BestDealCard from './components/BestDealCard.vue'
+import BrandCard from './components/BrandCard.vue'
 </script>
 
 <script>
@@ -55,6 +56,93 @@ export default {
           productReviewAverage: 4.5,
           productTotalReviews: 121,
           productPrice: 239
+        },
+        {
+          productId: 2,
+          productImgPath: 'bestdeal-1.png',
+          productName: 'HomePod mini',
+          productDescription: 'Table with air purifier, stained veneer/black',
+          productReviewAverage: 4.5,
+          productTotalReviews: 121,
+          productPrice: 239
+        },
+        {
+          productId: 2,
+          productImgPath: 'bestdeal-1.png',
+          productName: 'HomePod mini',
+          productDescription: 'Table with air purifier, stained veneer/black',
+          productReviewAverage: 4.5,
+          productTotalReviews: 121,
+          productPrice: 239
+        },
+        {
+          productId: 2,
+          productImgPath: 'bestdeal-1.png',
+          productName: 'HomePod mini',
+          productDescription: 'Table with air purifier, stained veneer/black',
+          productReviewAverage: 4.5,
+          productTotalReviews: 121,
+          productPrice: 239
+        },
+        {
+          productId: 2,
+          productImgPath: 'bestdeal-1.png',
+          productName: 'HomePod mini',
+          productDescription: 'Table with air purifier, stained veneer/black',
+          productReviewAverage: 4.5,
+          productTotalReviews: 121,
+          productPrice: 239
+        },
+        {
+          productId: 2,
+          productImgPath: 'bestdeal-1.png',
+          productName: 'HomePod mini',
+          productDescription: 'Table with air purifier, stained veneer/black',
+          productReviewAverage: 4.5,
+          productTotalReviews: 121,
+          productPrice: 239
+        }
+      ],
+      brands: [
+        {
+          brandId: 1,
+          brandTitle: 'Staples',
+          brandImgPath: 'brand_staples.png'
+        },
+        {
+          brandId: 2,
+          brandTitle: 'Sprouts',
+          brandImgPath: 'brand_sprouts.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
+        },
+        {
+          brandId: 3,
+          brandTitle: 'Grocery outlet',
+          brandImgPath: 'brand_grocery_outlet.png'
         }
       ]
     }
@@ -85,11 +173,12 @@ export default {
   <section class="best-deal-area">
     <div class="container">
       <div class="best-deal-title-wrap">
-        <h3 class="categories-title">Todays best deals for you!</h3>
+        <h3 class="best-deal-title">Todays best deals for you!</h3>
       </div>
       <div class="best-deal-wrap">
         <div class="best-deal-slider">
           <BestDealCard
+            class="best-deal-slide"
             v-for="(item, index) in bestDeals"
             :item="item"
             :index="index"
@@ -105,12 +194,65 @@ export default {
       </div>
     </div>
   </section>
+
+  <section class="brand-area">
+    <div class="container">
+      <div class="brand-title-wrap">
+        <h3 class="brand-title">Choose By Brand</h3>
+      </div>
+      <div class="brand-wrap">
+        <BrandCard
+          v-for="(item, index) in brands"
+          :item="item"
+          :index="index"
+          :key="item.brandId"
+          :brandTitle="item.brandTitle"
+          :brandImgPath="item.brandImgPath"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+.brand-wrap {
+  display: grid;
+  flex-wrap: nowrap;
+  grid-auto-columns: 1fr;
+  grid-column-gap: 23px;
+  grid-row-gap: 16px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto;
+}
+
+.best-deal-area {
+  padding-bottom: 80px;
+}
+.best-deal-slider .best-deal-slide {
+  flex-shrink: 0;
+  scroll-snap-align: center;
+}
+
+.best-deal-slider .best-deal-slide:last-child {
+  padding-right: 20px;
+}
+
 .best-deal-slider {
   display: flex;
+  /* -ms-overflow-style: none; */
+  scrollbar-width: none;
+  width: auto;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  display: flex;
+  align-items: stretch;
+  scroll-snap-type: x mandatory;
 }
+
+.best-deal-slider::-webkit-scrollbar {
+  /* display: none; */
+}
+
 .shop-category-wrap {
   display: grid;
   grid-auto-columns: 1fr;
@@ -123,7 +265,10 @@ export default {
   padding-top: 80px;
   padding-bottom: 80px;
 }
-.categories-title {
+
+.categories-title,
+.best-deal-title,
+.brand-title {
   margin-top: 20px;
   margin-bottom: 40px;
   font-family: Eina02, sans-serif;
---
+++ src/components/BrandCard.vue
@@ -0,0 +1,70 @@
+<script></script>
+
+<script setup>
+const props = defineProps({
+  brandTitle: String,
+  brandImgPath: String
+})
+
+const imageUrl = new URL(`/src/assets/images/${props.brandImgPath}`, import.meta.url).href
+</script>
+
+<template>
+  <div class="brand-item">
+    <div class="brand-image-wrap">
+      <img class="brand-image" :src="imageUrl" />
+    </div>
+    <div class="brand-content">
+      <h3 class="brand-item-title">{{ brandTitle }}</h3>
+      <div class="delivery-time">Delivery with in 24 hours</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.brand-item {
+  position: relative;
+  z-index: 0;
+  display: flex;
+  padding: 20px;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #f5f6f6;
+  border-radius: 10px;
+}
+
+.brand-image-wrap {
+  position: relative;
+  margin-right: 18px;
+}
+
+.brand-image {
+  border-radius: 10px;
+  max-width: 100%;
+}
+
+.brand-item-title {
+  margin-top: 0px;
+  margin-bottom: 13px;
+  font-family: Inter, sans-serif;
+  color: #231f1e;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.delivery-time {
+  margin-bottom: 0px;
+  font-size: 13px;
+  line-height: 13px;
+  font-weight: 500;
+}
+
+h3 {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  font-family: Eina02, sans-serif;
+  font-size: 28px;
+  line-height: 100%;
+  font-weight: 700;
+}
+</style>
